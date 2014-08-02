4ada31c97d2a16f9ebbd0bdf88699e876ec4f811
sriajay <itsurajaykumar@gmail.com>
2014-08-02 11:45:17 +0530

    Contains Index File

---
+++ SKart/src/com/alt/skart/MainActivity.java
@@ -0,0 +1,18 @@
+package com.alt.skart;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+//		setContentView(R.layout.activity_main);
+	}
+
+	
+
+}
